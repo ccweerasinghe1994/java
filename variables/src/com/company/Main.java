567e842270d82b9d661581863c42f0662fcca887
chamara chathuranga weerasinghe <ccweerasinghe1994@gmail.com>
2020-01-10 09:30:20 +0530

    first commit

---
+++ variables/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        variables are used to store information
+//        lets start with a basic one integer a whole number
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+
+//        mathematical operations can be done in the variables
+        int mySecondNumber =myFirstNumber+45;
+        System.out.println(mySecondNumber);
+
+    }
+}
