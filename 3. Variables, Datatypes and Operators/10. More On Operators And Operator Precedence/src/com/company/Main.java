37956a437d7b69562ba387eeb232b9d17e0fc2c1
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-05-18 12:01:54 +0530

    add image

--- 3. Variables, Datatypes and Operators/10. More On Operators And Operator Precedence/src/com/company/Main.java
+++ 3. Variables, Datatypes and Operators/10. More On Operators And Operator Precedence/src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int topScore = 85;
+        if (topScore < 100) {
+            System.out.println("You got a top score!");
+        }
+        int secondTopScore = 15;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Your score is higher than second top score lower than 100");
+        }
+
+        int newValue = 50;
+        if (newValue = 50)
+        {
+            System.out.println("This is a Error");
+        }
+
+        
     }
 }
---
+++ Docs/3.Variables,DatatypesandOperators/moreOperators.md
@@ -0,0 +1,5 @@
+# More Operators
+
+## Error
+
+![error](./img/4.png)
